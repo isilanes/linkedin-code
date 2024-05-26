84322e5bc0152f262e1a1661685db930ceb090a9
Iñaki Silanes <isilanes@gmail.com>
2024-05-26 21:32:21 +0200

    Implement and measure both proposed implementations

--- living_per_year.py
+++ living_per_year.py
@@ -1,3 +1,12 @@
+import collections
+import random
+import timeit
+
+
+def generate_population(age: int, years: int, individuals: int) -> list[tuple[int, int]]:
+    return [(b := random.randrange(0, years), b+age) for _ in range(individuals)]
+
+
 def f1(population):
     living_per_year = {}
     for birth, death in population:
@@ -9,6 +18,20 @@ def f1(population):
     return living_per_year
 
 
+def f2(population):
+    return collections.Counter(
+        year
+        for birth, death in population
+        for year in range(birth, death)
+    )
+
+
 if __name__ == "__main__":
-    p = [(1, 10)]
-    print(f1(p))
+    p = generate_population(age=250, years=10000, individuals=1000)
+
+    assert f1(p) == f2(p)
+
+    for fun in (f1, f2):
+        t = timeit.Timer(lambda: fun(p))
+        dt = t.timeit(10)/10  # seconds
+        print(f"{fun.__name__}: {dt*1000:.1f} ms")
